42c4a1780cfb065ce6fa74e0c3a70ff397c5d8c6
ZNguyenLe <nguyen.le@stclairconnect.ca>
2021-09-23 21:15:20 -0400

    Added Files related to Assignment 2

---
+++ Assignment 2/js/script.js
@@ -0,0 +1,21 @@
+// WEB303 Assignment 2
+// Nguyen Le
+
+$(document).ready(function() {
+    $("a#vprospect").click(function(){
+        $("div").load("prospect.html");
+        $("#solution").fadeIn();
+    });
+});
+
+$(document).ready(function() {
+    $("a#vconvert").click(function(){
+        $("div").load("convert.html");
+    });
+});
+
+$(document).ready(function() {
+    $("a#vretain").click(function(){
+        $("div").load("retain.html");
+    });
+});
