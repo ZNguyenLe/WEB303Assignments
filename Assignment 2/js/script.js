8cc96d28b5d19817403a44afe5faa6755e5dce22
ZNguyenLe <nguyen.le@stclairconnect.ca>
2021-09-24 01:33:06 -0400

    Added animations with JQuery

--- Assignment 2/js/script.js
+++ Assignment 2/js/script.js
@@ -4,18 +4,25 @@
 $(document).ready(function() {
     $("a#vprospect").click(function(){
         $("div").load("prospect.html");
+        $("div").slideToggle();
     });
 });
 
 $(document).ready(function() {
     $("a#vconvert").click(function(){
         $("div").load("convert.html");
+         $("div").animate({
+             height: 'toggle'
+         })
     });
 });
 
 $(document).ready(function() {
     $("a#vretain").click(function(){
         $("div").load("retain.html");
+         $("div").animate({
+             height: 'toggle'
+         })
     });
 });
 
