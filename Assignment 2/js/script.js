a323b71f8b23e4d6d7b1e026da386ccbd215b64e
ZNguyenLe <nguyen.le@stclairconnect.ca>
2021-09-24 01:43:04 -0400

    added comments
    Loaded external html files to index to display on the same page and added comments

--- Assignment 2/js/script.js
+++ Assignment 2/js/script.js
@@ -3,24 +3,24 @@
 
 $(document).ready(function() {
     $("a#vprospect").click(function(){
-        $("div").load("prospect.html");
-        $("div").slideToggle();
+        $("div").load("prospect.html");// loaded prospect.html onto index.html with ajax
+        $("div").slideToggle();        // animated using Jquery
     });
 });
 
 $(document).ready(function() {
     $("a#vconvert").click(function(){
-        $("div").load("convert.html");
-         $("div").animate({
+        $("div").load("convert.html");  // loaded convert.html onto index.html with ajax
+         $("div").animate({             // animated using the animate function with Jquery
              height: 'toggle'
          })
     });
 });
 
 $(document).ready(function() {
-    $("a#vretain").click(function(){
-        $("div").load("retain.html");
-         $("div").animate({
+    $("a#vretain").click(function(){ 
+        $("div").load("retain.html");   // loaded retain.html onto index.html with ajax
+         $("div").animate({             // animated using the animate function with Jquery
              height: 'toggle'
          })
     });
