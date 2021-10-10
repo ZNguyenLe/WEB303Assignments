fe6e940b92831ada00731735b9d3e36afd6a78b3
ZNguyenLe <nguyen.le@stclairconnect.ca>
2021-10-10 14:38:22 -0400

    added an if statement
    Added if statement to welcome user while being prompt to ask for location, also if denied, notify user that geolocation is unavailable. if enabled, display location coords

--- WEB303As04/script.js
+++ WEB303As04/script.js
@@ -10,24 +10,27 @@ $(document).ready(function(){
     let lat2 = localStorage.getItem('lat2');
     let lon2 = localStorage.getItem('lon2'); 
 
+    if(localStorage.getItem(lat1) !== null) {        
+        $('#youarehere').append("<h2>Welcome to the Geolocation Page</h2>");                    //checks to see if there is localstorage stored
+    };
         navigator.geolocation.getCurrentPosition((position) => {
-        $('#youarehere').append("<h2>You are here at this Current Location</h2>" + position.coords.latitude + " , " + position.coords.longitude);
-            console.log('geolocation is showing');
+        $('#youarehere').append("<h2>You are here at this Current Location</h2>" + position.coords.latitude + " , " + position.coords.longitude); //if geolocation is available. console log it
+            console.log('The Geolocation you are trying to access is available');
 
-            if(!localStorage.getItem(lat1) && !localStorage.getItem(lon1)) {
+            if(!localStorage.getItem(lat1) && !localStorage.getItem(lon1)) {    //c
                 localStorage.setItem(lat1, position.coords.latitude);
-                localStorage.setItem(lon1, position.coords.longitude);        
-                 $('#youarehere').append("<h2>Compared lat 1 and lon 1</h2>");
-            }   else 
-            if(localStorage.getItem(lat1) && localStorage.getItem(lon1)) {
-                $('#youarehere').append('<h2>Your previous location was at </h2>' + position.coords.latitude + " , " + position.coords.longitude);
-                localStorage.setItem(lat2, position.coords.latitude);
-                localStorage.setItem(lon2, position.coords.longitude); 
-                $('#youarehere').append("<h2>You have moved a Distance of </h2>" + calcDistance(lat1, lon1, lat2, lon2));  
+                localStorage.setItem(lon1, position.coords.longitude);
+            }   
+            else if(localStorage.getItem(lat1) && localStorage.getItem(lon1)) {
+                    $('#youarehere').append('<h2>Your previous location was at </h2>' + position.coords.latitude + " , " + position.coords.longitude);
+                    localStorage.setItem(lat2, position.coords.latitude);
+                    localStorage.setItem(lon2, position.coords.longitude); 
+                    $('#youarehere').append("<h2>You have moved a Distance of </h2>" + calcDistance(lat1, lon1, lat2, lon2));  
         }
     }, () => {
-       $('#youarehere').append("<h2>The Geolocation you are trying to access is unavailable</h2>");
+       $('#youarehere').append("<h3>The Geolocation you are trying to access is not available. Please allow location in order for the page to work.</h3>");
         console.log('The Geolocation you are trying to access is not available');
+        console.log('There is no Local Storage');                               //displays in the console log that there is no locale storage stored
     }); 
 
     // function to calculate the distance in metres between two lat/long pairs on Earth
