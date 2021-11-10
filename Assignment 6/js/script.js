478392bca6b02967c0a76a1854508ad13053c5c6
ZNguyenLe <90646702+ZNguyenLe@users.noreply.github.com>
2021-11-09 21:19:04 -0500

    added images / page reference where images came from.

--- Assignment 6/js/script.js
+++ Assignment 6/js/script.js
@@ -1,29 +1,23 @@
 $(document).ready(function() {
         $('#accordion').on('click', '#control', function () {
-            $(this).next().addClass('active').slideDown(1000); //add class and slide down and show content
-            $('.active').not($(this).next()).removeClass('active').slideUp(1000); // remove class and slide up and hide content
+            $(this).next().addClass('active3').slideDown(1000); //add class and slide down and show content
+            $('.active3').not($(this).next()).removeClass('active3').slideUp(1000); // remove class and slide up and hide content
             $(this).next('#apanel').show(); // if another panel is clicked, repeat lines 3 and 4
             console.log('please work qwq'); // crying for this code to work. 
     });
-    $('#accordion2').on('click', '#control2', function () {
-        $(this).next().addClass('active2').slideDown(1000); //add class and slide down and show content
-        $('.active2').not($(this).next()).removeClass('active2').slideUp(1000); // remove class and slide up and hide content
-        $(this).next('#apanel2').show(); // if another panel is clicked, repeat lines 3 and 4
-        console.log('please work qwq on acc2'); // crying for this code to work. 
-});
-        // $('#accordion2').click(function() {
-        //     $(this)
-        //     .toggleClass('active')
-        //     .next("#apanel2")
-        //     .slideToggle(1000);
-        // });
+        $('#accordion2').on('click', '#control2', function () {
+            $(this).next().addClass('active2').slideDown(1000); //add class and slide down and show content
+            $('.active2').not($(this).next()).removeClass('active2').slideUp(1000); // remove class and slide up and hide content
+            $(this).next('#apanel2').show(); // if another panel is clicked, repeat lines 3 and 4
+            console.log('please work qwq on acc2'); // crying for this code to work. 
+    });
 });
 
 $(document).ready(function() {
-    $('#tablist').each(function() {
+
         let $this = $(this);                    // determines 'this' item
-        let $tabbed = $this.find('li.active');  // finds the li tag that is active
-        let $link = $tabbed.find('a');          // finds the link
+        let $li = $this.find('li.active');  // finds the li tag that is active
+        let $link = $li.find('a');          // finds the link
         let $active = $($link.attr('href'));    // this grabs the active panel
         
         $this.on('click', '#controltab', (function(n) {
@@ -33,9 +27,8 @@ $(document).ready(function() {
 
             if (clicked && !$link.parent().is('active'))
             $active.removeClass('active'); 
-            $tabbed.removeClass('active'); 
+            $li.removeClass('active'); 
             $active = $(clicked).addClass('active');
             $tab = $link.parent().addClass('active');
         }));
-    });
-});
+     });
